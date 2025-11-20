b0cfc331db73fe0ae0aadbd1c24e5a4b45ad411c
DanielBerg <daniel.berg@l58.se>
2025-11-20 15:32:10 +0100

    init push

---
+++ src/Server/SignalRc.Server.Host.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SignalRc.Server.Host.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+        }
+    }
+}
